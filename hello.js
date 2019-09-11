8d6d7b913223612f82742d9e39cbba608ea672da
Nathaniel Cox <nathanielacox@gmail.com>
2019-09-11 13:13:21 -0400

    Chapter 4

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("Hello, world!", "how's it going?");
+const daysOfTheWeek = ["Sunday, Monday, Tuesday", "Wednesday",
+                       "Thurdsy", "Friday", "Saturday"];
+
+let now = new Date();
+let dayName = daysOfTheWeek[now.getDay()];
+alert(`Hello, world! Happy $(dayName).`);
